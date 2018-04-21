55f4da2bd2103aa488c2686ef10cc0fde6a65cf7
Asuis <727443530@qq.com>
2018-04-21 10:15:51 +0800

    heelo

---
+++ src/main/java/com/real/centerserver/CenterServerApplication.java
@@ -0,0 +1,17 @@
+package com.real.centerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author asuis
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CenterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterServerApplication.class, args);
+	}
+}
